68d7d774036d162072b1426075edd9dcd4faf034
Klaudia Antczak <klaantczak@gmail.com>
2017-06-01 13:45:47 +0200

    search form completed

--- app/models/menu.rb
+++ app/models/menu.rb
@@ -1,5 +1,10 @@
 class Menu < ApplicationRecord
   belongs_to :chef
   validates :name, :description, :meal_type, :price_per_guest, presence: true
-  validates :meal_type, inclusion: { in: %w(Italian Indian Chinese Thai Vietnamese Mexican Greek Lebanese Korean American) }
+
+  after_validation :upcase_meal_type
+
+  def upcase_meal_type
+    self.meal_type = meal_type.capitalize
+  end
 end
