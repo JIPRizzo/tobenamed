ef1d83da6ea8291df0ed19bff9835446d0bdce50
joseportillo <jiprizzo@gmail.com>
2017-06-01 10:44:25 +0200

    WIP

--- config/routes.rb
+++ config/routes.rb
@@ -1,40 +1,8 @@
 Rails.application.routes.draw do
-  resources: orders do
-    resources: chefs
-
-  get 'orders/new'
-
-  get 'orders/create'
-
-  get 'orders/destroy'
-
-  get 'orders/index'
-
-  get 'orders/update'
-
-  get 'orders/edit'
-
-  get 'orders/add'
-
-  get 'menus/new'
-
-  get 'menus/create'
-
-  get 'menus/index'
-
-  get 'menus/destroy'
-
-  get 'menus/update'
-
-  get 'menus/edit'
-
-  get 'menus/add'
-
-  get 'chefs/create'
-
-  get 'chefs/new'
-
+  resources :orders do
+    resources :chefs
+  end
   devise_for :users
   root to: 'pages#home'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
