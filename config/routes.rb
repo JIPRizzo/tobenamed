c9d4c8cff3b0df38242a135f20091954333e52d3
joseportillo <jiprizzo@gmail.com>
2017-06-01 11:38:57 +0200

    changes to routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,8 @@
 Rails.application.routes.draw do
 
   resources :chefs
-  master
 
   devise_for :users
   root to: 'pages#home'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
 
