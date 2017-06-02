cc4f4ec875b9ec89c5adb8b1e7ee5954806bf4f8
Benoit Ladure <benoitladure@yahoo.com>
2017-06-02 15:46:32 +0200

    cards on index

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
 ActiveRecord::Schema.define(version: 20170601170451) do
-=======
-ActiveRecord::Schema.define(version: 20170601115105) do
->>>>>>> master
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -79,8 +75,6 @@ ActiveRecord::Schema.define(version: 20170601115105) do
     t.inet     "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
-    t.string   "first_name"
-    t.string   "last_name"
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
